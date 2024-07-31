e35126fde0e1011416bedbabe4f5aff8661c308f
mao <maonianyou@foxmail.com>
2024-07-31 14:22:18 +0800

    feat: github action for gh-pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 
+const isProd = process.env.NODE_ENV === 'production';
+console.log('isProd', isProd);
+
 export default defineConfig({
   server: {
     port: 30001,
   },
+  base: isProd ? '/demo-css-transform/' : '',
 });
